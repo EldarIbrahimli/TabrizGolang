9fe21c45f7aef81d1f42257816c4662d9a38b012
EldarIbrahimli <elik.khalilov@gmail.com>
2024-11-20 17:38:51 +0300

    Addet calc

--- Calculator/calculator.go
+++ Calculator/calculator.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2, num3, sum int
+	//did not do it myself could not understand by myself
+	var number int
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&number)
+
+	sum := 0
+	for number != 0 {
+		sum += number % 10 // Add the last digit to sum
+		number /= 10       // Remove the last digit
+	}
+
+	fmt.Println("Sum of digits:", sum)
+
+	/*var num1, num2, num3, sum int
 	fmt.Println("Write three numbers")
 	fmt.Scan(&num1, &num2, &num3)
 	sum = num1 + num2 + num3
-	fmt.Println(sum)
+	fmt.Println(sum)*/
 
 }
