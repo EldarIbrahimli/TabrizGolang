2299eb5135fee6527158af73fe341f6cca6a7754
EldarIbrahimli <elik.khalilov@gmail.com>
2024-11-24 13:01:05 +0300

    new Api Todo list and a summation calculator

---
+++ API_Calculator/calcAPI.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sum(context *gin.Context) {
+
+	n1 := context.Query("n1")
+	n2 := context.Query("n1")
+
+	num1, err := strconv.Atoi(n1)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid first number",
+		})
+		return
+	}
+
+	num2, err := strconv.Atoi(n2)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid second number",
+		})
+		return
+	}
+
+	sum := num1 + num2
+
+	context.JSON(http.StatusOK, gin.H{
+		"result": sum,
+	})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/sum", sum)
+	router.Run("localhost:3000")
+}
